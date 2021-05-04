4fe850dd533e10fa337bc9d36df827ebb8f9e4b3
SHAMEEL AHAMMED MP <63732845+mpshameel@users.noreply.github.com>
2021-05-04 08:03:26 +0530

    Update settings.py

--- Ebro/settings.py
+++ Ebro/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
@@ -148,7 +148,7 @@ STATICFILES_DIRS=[
 
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
