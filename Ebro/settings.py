437ca793700a773dd5e190f251431aa09ab1fbf2
mp_shameel <shameelahmedmp@gmail.com>
2021-06-12 09:53:44 +0530

    f

--- Ebro/settings.py
+++ Ebro/settings.py
@@ -92,6 +92,17 @@ TEMPLATES = [
 ]
 
 
+
+
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'allauth.account.context_processors.account',
+    'allauth.socialaccount.context_processors.socialaccount'
+)
+
+
 AUTHENTICATION_BACKENDS = [
 
     'django.contrib.auth.backends.ModelBackend',
