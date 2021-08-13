fbf31d41dfe5e87a7e3caa2cc7bd177f8fe7f3c0
mp_shameel <shameelahmedmp@gmail.com>
2021-08-14 01:58:38 +0530

    first commit

--- admin_phase/models.py
+++ admin_phase/models.py
@@ -292,24 +292,7 @@ class products(models.Model):
     
 
 
-    # nome = models.CharField(max_length=200, null=False, blank=False)
-    # apresentacao = models.TextField(null=False, blank=False)
-    # foto = CloudinaryField('foto', null=True, blank=True)
-    # created_date = models.DateTimeField(
-    #         default=timezone.now)
-    # published_date = models.DateTimeField(
-    #         blank=True, null=True)
 
-    # def published(self):
-    #     self.published_date = timezone.now()
-    #     self.save()
-
-    # def __str__(self):
-    #     return self.nome
-
-    # def get_absolute_url(self):
-    #     #return reverse("detalhe", kwargs={"pk": self.pk})
-    #     return "/post/%s" %(self.pk)
 
 
 
